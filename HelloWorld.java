a5df98169453f4122206dd137f883897e3d2c2ff
Sunk1123 <31610979+Sunk1123@users.noreply.github.com>
2017-09-11 14:21:04 +0800

    hahaha

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class HelloWorld{
+	 public static void main(String[] args) {
+	 	String name="sunk";
+	 	String String="111";
+       String String="222";
+       String args="333";
+       String Hello_World="444";
+       String $wreq="555";
+       String _qwer="666";
+       String protected="777";
+       String false="888";
+       String synchronized="999";
+
+	 		//用来输出括号之间包裹的内容
+		    System.out.println(name);
+		    System.out.println(String);
+ 			System.out.println(args);
+ 			System.out.println(Hello_World);
+ 			System.out.println($wreq);
+ 			System.out.println(_qwer);
+ 			System.out.println(protected);
+ 			 System.out.println(false);
+ 			System.out.println(synchronized);
+		
+
+	}
+}
